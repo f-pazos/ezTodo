e8337b549a2dc66e1e79996f1c2bde3b1be32bee
Felipe Pazos <gadzookese@gmail.com>
2017-07-09 11:55:18 -0400

    Started work on new todo prompt

--- javascript.js
+++ javascript.js
@@ -8,23 +8,41 @@ app.controller('myCtrl', function($scope, $mdDialog) {
 
 	$scope.todos = []
 
-	$scope.todos.push( {title: "DO THIS THING", description: "you really need to do this thing", deadline: "TWO HOURS"} )
+	$scope.todos.push( {title: "asdfghjkl asdfghjkl asdfghjkl asdfghjkl asdfghjkl asdfghjkl ", description: "you really need to do this thing", deadline: "TWO HOURS"} )
+	$scope.todos.push( {title: "asldkfj", description: "a;sldkjfads", deadline: "a;sdlkfjasdf" } )
 
 	$scope.addTodo = function(){
 		console.log( "test" );
 		$scope.todos[0].title = "yay you did";
 	}
 
-	$scope.showAlert = function( ev ){
-		$mdDialog.show( 
-			$mdDialog.alert()
-			.parent(angular.element( document.querySelector( '#popupContainer' ) ) )
-			.clickOutsideToClose(true)
-			.title( 'This is an alert title' )
-			.textContent( 'you can specifty ' )
-			.ariaLabel( 'Alert Dialog Demo' )
-			.ok( 'Got it' )
-			.targetEvent( ev )
-			);
+	$scope.showNewTodoPrompt = function( ev ){
+		$mdDialog.show( {
+			controller: DialogController,
+			templateUrl: 'newTodoTemp.html',
+			parent: angular.element( document.body ),
+			targetEvent: ev, 
+			clickOutsideToClose: true,
+			fullscreen: $scope.customeFullscreen 
+			})
+			.then( function( answer ) {
+				$scope.status = 'You said the informtion was " ' + answer + '". ';
+			}, function() {
+				$scope.status = "you cancelled the dialog.";
+			});
 	};
+
+	function DialogController( $scope, $mdDialog ) {
+		$scope.hide = function(){
+			$mdDialog.hide();
+		};
+
+		$scope.cancel = function() {
+			$mdDialog.cancel();
+		};
+
+		$scope.answer = function( answer ) {
+			$mdDialog.hide( answer );
+		}
+	}
 });
